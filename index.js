3a52e991b1422b57f64706105481a27340266361
locusarthurpereira <arthurgabrielpereira@hotmail.com>
2022-03-29 17:55:32 -0300

    Atualização do index.js

--- index.js
+++ index.js
@@ -8,31 +8,10 @@ const server = http.createServer((req, res) => {
     const reqBreaked = req.url.split("?");
 
     if (reqBreaked[0] === "/decode" && req.method == "POST") {
-
-        let body = '';
-        req.on('data', function (chunk) {
-            body += chunk;
-        });
-
-        req.on('end', () => {
-            var jsonBody = JSON.parse(body);
-            res.end("A String é traduzida como: " + invertAndConvert(jsonBody.encoded));
-        });
-
+        postConvertString(req, res);
     } 
     else if(reqBreaked[0] === "/decode" && req.method == "GET") {
-        var data = reqBreaked[1].split("=")
-
-        res.setHeader('Content-Type', 'application/json');
-
-        if (data.length > 2) {
-            res.statusCode = 400;
-            res.end('É preciso passar apenas um parâmetro');
-            return
-        }
-
-        res.statusCode = 200;
-        res.end("A String é traduzida como: " + invertAndConvert(data[1]));
+        getConvertString(res, reqBreaked);
     } 
     else {
         res.statusCode = 404;
@@ -44,6 +23,47 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Servidor esta em http://${hostname}:${port}/`);
 });
+
+function postConvertString(req, res) {
+
+    let body = '';
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+
+    req.on('end', () => {
+        try {
+            var jsonBody = JSON.parse(body);
+            res.end("A String é traduzida como: " + invertAndConvert(jsonBody.encoded));
+        } catch {
+            res.statusCode = 400;
+            res.end('É preciso passar pelo menos um parâmetro');
+            return
+        }
+    });
+}
+
+function getConvertString(res, reqBreaked) {
+    try{
+        var data = reqBreaked[1].split("=")
+
+        res.setHeader('Content-Type', 'application/json');
+    
+        if (data.length > 2) {
+            res.statusCode = 400;
+            res.end('É preciso passar apenas um parâmetro');
+            return
+        }
+    
+        res.statusCode = 200;
+        res.end("A String é traduzida como: " + invertAndConvert(data[1]));
+    } catch {
+        res.statusCode = 400;
+        res.end('Error');
+        return
+    }
+
+}
 
 function convertIntoString(text, finalText = '') {
     if (!text.length) {
